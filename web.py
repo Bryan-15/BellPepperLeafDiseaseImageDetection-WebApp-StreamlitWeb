d9df49d432de190cf1a4ae4edd1ff554ffae5a5d
Bryan-15 <76427500+Bryan-15@users.noreply.github.com>
2021-11-09 18:20:23 +0800

    Update web.py

--- web.py
+++ web.py
@@ -6,10 +6,8 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 import time
-import state
-from state import count_sessions
 from PIL import Image
-count_sessions()
+
 st.set_page_config(page_title='BPLDID', page_icon='🍃', layout='wide')
 st.title("BELL PEPPER LEAF DISEASE IMAGE DETECTION WEB SYSTEM")
 st.markdown('##')
