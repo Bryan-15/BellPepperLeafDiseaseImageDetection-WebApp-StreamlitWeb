51d5c6a72415bce0ce996badc3fba58472fa0da6
Bryan-15 <76427500+Bryan-15@users.noreply.github.com>
2021-11-10 17:11:38 +0800

    Update web.py

--- web.py
+++ web.py
@@ -44,41 +44,41 @@ if choice == "HOME":
         </style>
         """, unsafe_allow_html=True)
 
-        st.markdown("<h1 style='text-align: center; color: red;'>WELCOME TO THE HOME PAGE</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO THE HOME PAGE</h1>", unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h1 style='text-align: center; color: red;'>BELL PEPPER LEAF DISEASE INFORMATIONS</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>BELL PEPPER LEAF DISEASE INFORMATIONS</h1>", unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h1 style='text-align: center; color: red;'>Bacterial Spots</h1>", unsafe_allow_html=True)
-        st.markdown("<h2 style='text-align: center; color: red;'>Description </h2>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>Bacterial Spots</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Description </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Bacterial spot is one of the most devastating diseases of pepper and tomato. The disease occurs worldwide where pepper and tomato are grown in warm, moist areas. When it occurs soon after transplanting and weather conditions remain favorable for disease development, the results are usually total crop loss.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Cause </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Cause </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Bacterial leaf spot on peppers is a devastating disease that can cause disfiguration of the leaves and fruit. In severe cases, the plants may die. There is no cure once the disease takes hold, but there are several things you can do to prevent it and keep it from spreading, Causes lesions on the leaves that look as though they are soaked with water. These lesions normally begin on the lower leaves. As the disease progresses, it leaves a dark, purple-brown spot with a light brown center. Bacterial leaf spot on peppers causes spotting and raised cracks in the fruit. The cracks provide an opening for other disease pathogens.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Solution/Treatment </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Solution/Treatment </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Seed can be treated with hot water or calcium hypochlorite to kill the pathogen. Hot water treatment is more thorough than calcium hypochlorite because it can kill bacteria inside the seed as well as those on the surface.</p>', unsafe_allow_html=True)
         st.markdown('##')
 
         st.markdown('##')
-        st.markdown("<h1 style='text-align: center; color: red;'>Cercospora Leaf Spots</h1>", unsafe_allow_html=True)
-        st.markdown("<h2 style='text-align: center; color: red;'>Description </h2>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>Cercospora Leaf Spots</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Description </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Any of several fungus diseases of plants caused by Cercospora species and characterized by areas of discoloration on the leaves. as individual, circular spots that are tan to light brown with reddish purple borders. As the disease progresses, individual spots coalesce. Heavily infected leaves first become yellow and eventually turn brown and necrotic.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Cause </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Cause </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">This disease is caused by the fungus Cercospora hydrangea and is perhaps the most common disease seen on this ornamental during the months of July through October. The disease rarely kills the plant, but if it is severe, it can reduce overall plant vigor by repeated defoliation.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Solution/Treatment </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Solution/Treatment </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">One of them is to Avoid overwatering or watering in the late evening to reduce free moisture. Another is to Avoid overhead watering where the water can dislodge and disperse spores to uninfected plants. Or space out the plants to encourage air movement and reduce high humidity levels.</p>', unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('##')
-        st.markdown("<h1 style='text-align: center; color: red;'>Powdery Mildew</h1>", unsafe_allow_html=True)
-        st.markdown("<h2 style='text-align: center; color: red;'>Description </h2>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>Powdery Mildew</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Description </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Powdery mildew, plant disease of worldwide occurrence that causes a powdery growth on the surface of leaves, buds, young shoots, fruits, and flowers. Powdery mildew is causedby many specialized races of fungal species in the genera Erysiphe, Microsphaera, Phyllactinia, Podosphaera, Sphaerotheca, and Uncinula.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Cause </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Cause </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Powdery mildew, mainly caused by the fungus Podosphaera xanthii, infects all cucurbits, including muskmelons, squash, cucumbers, gourds, watermelons and pumpkins. In severe cases, powdery mildew can cause premature death of leaves, and reduce yield and fruit quality.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Solution/Treatment </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Solution/Treatment </h2>", unsafe_allow_html=True)
         st.markdown('<p class="big-font">Combine one tablespoon baking soda and one-half teaspoon of liquid, non-detergent soap with one gallon of water, and spray the mixture liberally on the plants. Mouthwash. The mouthwash you may use on a daily basis for killing the germs in your mouth can also be effective at killing powdery mildew spores. Also along with using Fungicides that are highly effective with low toxicity, no residue, and long duration.</p>', unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('##')
@@ -89,6 +89,12 @@ if choice == "IMAGE DETECTION":
         time.sleep(7)
         st.header("BELL PEPPER LEAF DISEASE IMAGE DETECTION")
         st.markdown('##')
+        main_container = st.container()
+        if "counter" not in st.session_state:
+            st.session_state.counter = 0
+        #if st.button("Reset"):
+            #st.session_state.counter = 0    
+
         model = tf.keras.models.load_model("saved_model/mdl_wt.hdf5") 
 
         uploaded_file = st.file_uploader("Choose a image file", type="jpg")
@@ -115,6 +121,8 @@ if choice == "IMAGE DETECTION":
             if Genrate_pred:
                 prediction = model.predict(img_reshape).argmax()
                 st.title("The Image Result Is {}".format(map_dict [prediction]))
+                main_container.write(st.session_state.counter)
+                st.session_state.counter += 1
 
 if choice=="ABOUT US":
     with st.spinner('Loading Please Wait...'):
@@ -131,31 +139,31 @@ if choice=="ABOUT US":
         </style>
         """, unsafe_allow_html=True)
 
-        st.markdown("<h1 style='text-align: center; color: red;'>WELCOME TO ABOUT US PAGE</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO ABOUT US PAGE</h1>", unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>About Us</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>About Us</h2>", unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('<p class="big-font">This web system is a system entitled Bell Pepper Leaf Disease Image Detection B-P-L-D-I-D which purpose is to help user to easily identify what kind of bell pepper leaf disease their photo have. This system is made to help user to lessen the hassle of identifying those kinds of bell pepper leaf disease by themselves. Here in this Web System it is composed of the Home page, Image Detection page, About page and Contact page.</p>', unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('##')
 
-        st.markdown("<h1 style='text-align: center; color: red;'>System Features:</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>System Features:</h1>", unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Home Page:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Home Page:</h2>", unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('<p class="big-font">This is the main page and front page of the web system where you can move to other forms using the navigation bar and also in the main page is where you can find informations about the bell pepper leaf diseases.</p>', unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Image Detection Page:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Image Detection Page:</h2>", unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('<p class="big-font">In this page of the system is where the users can input images of bell pepper leaf in order to identify whether it is healthy or what kind of disease the bell pepper leaf image have. Futhermore, it also have a navigation bar within this page that can allow users to go to other forms of the system.</p>', unsafe_allow_html=True)
         st.markdown('##')
 
-        st.markdown("<h2 style='text-align: center; color: red;'>About Page:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>About Page:</h2>", unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('<p class="big-font">In this page it gives brief explanation of every features of this web system including the developers information. Futhermore, it also have a navigation bar within this page that can allow users to go to other forms of the system.</p>', unsafe_allow_html=True)
         st.markdown('##')
 
-        st.markdown("<h2 style='text-align: center; color: red;'>Contact Page:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Contact Page:</h2>", unsafe_allow_html=True)
         st.markdown('##')
         st.markdown('<p class="big-font">In this page you can find all the contacts that you can use to reach each of the people and developers behind this web system. Futhermore, it also have a navigation bar within this page that can allow users to go to other forms of the system.</p>', unsafe_allow_html=True)
         st.markdown('##')
@@ -176,13 +184,13 @@ if choice=="CONTACT":
         </style>
         """, unsafe_allow_html=True)
 
-        st.markdown("<h1 style='text-align: center; color: red;'>WELCOME TO CONTACT PAGE</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: white;'>WELCOME TO CONTACT PAGE</h1>", unsafe_allow_html=True)
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>YOU CAN CONTACT US THROUGH</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>YOU CAN CONTACT US THROUGH</h2>", unsafe_allow_html=True)
         st.markdown('##')
 
         
-        st.markdown("<h2 style='text-align: center; color: red;'>Contacts For:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Contacts For:</h2>", unsafe_allow_html=True)
         st.subheader("Bryan Christopher C. Colis")
         st.markdown('##')
         b=st.selectbox("Contacts:",["Click Here","Phone Number","Personal Email","Gsuite Email"])
@@ -200,7 +208,7 @@ if choice=="CONTACT":
             st.markdown('<p class="big-font">bryanchristopher.colis@g.batstate-u.edu.ph</p>', unsafe_allow_html=True)
 
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Contacts For:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Contacts For:</h2>", unsafe_allow_html=True)
         st.subheader("Denzl Eian A. Magtalas")
         st.markdown('##')
         d=st.selectbox("Contacts:",["Click  Here","Phone  Number","Personal  Email","Gsuite   Email"])
@@ -219,7 +227,7 @@ if choice=="CONTACT":
 
 
         st.markdown('##')
-        st.markdown("<h2 style='text-align: center; color: red;'>Contacts For:</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Contacts For:</h2>", unsafe_allow_html=True)
         st.subheader("Rogell A. Gutierrez ")
         st.markdown('##')        
         r=st.selectbox("Contacts:",["Click   Here","Phone   Number","Personal   Email","Gsuite   Email"])
